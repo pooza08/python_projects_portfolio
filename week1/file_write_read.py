2fe92ce971d3276323bec5a37cf830e123c84f81
Pooza K <kpooza03@gmail.com>
2025-08-20 23:39:43 +0530

    added week1 projects

--- week1/file_write_read.py
+++ week1/file_write_read.py
@@ -1,1 +1,7 @@
-
+with open("tasks.txt", "w") as f:
+    f.write("tasks completed. \n")
+with open("tasks.txt", "a") as f:
+    f.write("verified. \n")
+with open("tasks.txt", "r") as f:
+   content = f.read()
+   print("file content. \n", content)
--- week1/student_record_system.py
+++ week1/student_record_system.py
@@ -1,1 +1,10 @@
-
+students=[]
+def add_student(name,age,course):
+    student={"name":name,"age":age,"course":course}
+    students.append(student)
+def display_student():
+    for s in students:
+        print(f"Name:{s['name']},Age:{s['age']},Course:{s['course']}")
+add_student("pooza",20,"information technology")
+add_student("nila",20,"computer sicence")
+display_student()
